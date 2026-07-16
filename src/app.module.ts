b06c26e53a9ae72b3774e75492010abe12889f0d
Everilll <averil_mauladani_33rpl@student.smktelkom-mlg.sch.id>
2026-07-17 00:51:36 +0700

    feat: initialize directory structure for all core modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,17 @@ import { AppConfigModule } from './common/config/app-config.module';
 import { HashingModule } from './common/hashing/hashing.module';
 import { UploadModule } from './upload/upload.module';
 import { authEnvSchema } from './common/config/auth-env.schema';
+import { AuthModule } from './auth/auth.module';
+import { StudentsModule } from './students/students.module';
+import { ClassesModule } from './classes/classes.module';
+import { EventsModule } from './events/events.module';
+import { AnnouncementsModule } from './announcements/announcements.module';
+import { ExportModule } from './export/export.module';
+import { TeamsModule } from './teams/teams.module';
+import { RegistrationsModule } from './registrations/registrations.module';
 
 @Module({
-  imports: [AppConfigModule.forProject(authEnvSchema), HashingModule, PrismaModule, UploadModule],
+  imports: [AppConfigModule.forProject(authEnvSchema), HashingModule, PrismaModule, UploadModule, AuthModule, StudentsModule, ClassesModule, EventsModule, AnnouncementsModule, ExportModule, TeamsModule, RegistrationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
